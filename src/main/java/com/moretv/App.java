6b1a79e4045ac7821c8543e5ba55f385acf8856c
dava060 <dava060@126.com>
2016-08-28 07:52:07 +0800

    更新app.java 文件

--- src/main/java/com/moretv/App.java
+++ src/main/java/com/moretv/App.java
@@ -18,8 +18,6 @@ public class App {
     public static AbstractApplicationContext ctx = null;
 
     public static void main(String[] args) throws Exception {
-//        FileSystemXmlApplicationContext ctx = new FileSystemXmlApplicationContext(new String[] {"file:/Users/armstrong/work/java/helios_messagebus/resource/applicationContext.xml", "file:/Users/armstrong/work/java/helios_messagebus/resource/applicationContext-jedis.xml", "file:/Users/armstrong/work/java/helios_messagebus/resource/applicationContext-rabbitmq.xml" });
-//default:      FileSystemXmlApplicationContext ctx = new FileSystemXmlApplicationContext(new String[] { "file:/home/putao/code/java_code/helios_messagebus/config/applicationContext.xml", "file:/home/putao/code/java_code/helios_messagebus/config/applicationContext-jedis.xml", "file:/home/putao/code/java_code/helios_messagebus/config/applicationContext-rabbitmq.xml" });
 
         // config path, TODO:改成生产环境的,
         //String configFileCatalog = "F:/test/boss_messagebus/resource/";//本机环境：：
